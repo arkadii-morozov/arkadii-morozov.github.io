7dca0322aafea64b869a7e7e7265b0c6844aed76
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-06-29 12:15:45 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,14 +1,13 @@
 window.onload = init();
 // var i =0;
-var count = false;
-var drop = document.querySelector(".drop-nav");
-var btn = document.querySelector(".menu__btn");
-
 function init(){
-	btn.onclick = dropMenu();
+	var count = false;
+	var drop = document.querySelector(".drop-nav");
+	var btn = document.querySelector(".menu__btn");
+	btn.onclick = dropMenu(drop,count);
 }
 
-function dropMenu(){
+function dropMenu(drop,count){
 	console.log(count);
 	console.log(drop);
 	count = !count;
@@ -26,3 +25,19 @@ function dropMenu(){
 		drop.style.zIndex = "0";
 	}
 } 
+
+// $(window).on( "load", init);
+
+// function init(){
+//     var count = false;
+//     $('.sub_catalog').on('click', () =>{
+//         count = !count;
+//         if(count){
+//             $(".block_list_cat").css({'display':'block'});    
+//             $(".button_catalog").css({'display':'block'});    
+//         } else{
+//             $(".block_list_cat").css({'display':'none'});    
+//             $(".button_catalog").css({'display':'none'});    
+//         }
+//         console.log(count);
+//     });
