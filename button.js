67c1e99122406199f91388a881627de473687b10
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-06-29 12:04:38 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,14 +1,16 @@
-window.onload = init;
+window.onload = init();
 // var i =0;
 var count = false;
 var drop = document.querySelector(".drop-nav");
+var btn = document.querySelector(".menu__btn");
+
 function init(){
-	var btn = document.querySelector(".menu__btn");
 	btn.onclick = dropMenu();
 }
 
 function dropMenu(){
 	console.log(count);
+	console.log(drop);
 	count = !count;
 // 	var window_size = window.matchMedia('(max-width: 768px)');
 	if (count && window.matchMedia('(max-width: 768px)').matches){
