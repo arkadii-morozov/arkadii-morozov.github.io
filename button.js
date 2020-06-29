599329493bca5de5983d75a53fe71cde3f5e630f
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-06-29 12:32:10 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,43 +1,41 @@
-window.onload = init();
-// var i =0;
-function init(){
-	var count = false;
-	var btn = document.querySelector(".menu__btn");
-	btn.onclick = dropMenu(count);
-}
-
-function dropMenu(count){
-	var drop = document.querySelector(".drop-nav");
-	console.log(count);
-	console.log(drop);
-	count = !count;
-// 	var window_size = window.matchMedia('(max-width: 768px)');
-	if (count && window.matchMedia('(max-width: 768px)').matches){
-// 		var but = document.querySelector("#button");
-// 		but.setAttribute("checked", "checked");
-		drop.style.display = "block";
-		drop.style.zIndex = "1000";
-	}
-	else {
-// 		var but = document.querySelector("#button");
-// 		but.setAttribute("checked", "false");
-		drop.style.display = "none";
-		drop.style.zIndex = "0";
-	}
-} 
-
-// $(window).on( "load", init);
-
+// window.onload = init();
+// // var i =0;
 // function init(){
-//     var count = false;
-//     $('.sub_catalog').on('click', () =>{
-//         count = !count;
-//         if(count){
-//             $(".block_list_cat").css({'display':'block'});    
-//             $(".button_catalog").css({'display':'block'});    
-//         } else{
-//             $(".block_list_cat").css({'display':'none'});    
-//             $(".button_catalog").css({'display':'none'});    
-//         }
-//         console.log(count);
-//     });
+// 	var count = false;
+// 	var btn = document.querySelector(".menu__btn");
+// 	btn.onclick = dropMenu(count);
+// }
+
+// function dropMenu(count){
+// 	var drop = document.querySelector(".drop-nav");
+// 	console.log(count);
+// 	console.log(drop);
+// 	count = !count;
+// // 	var window_size = window.matchMedia('(max-width: 768px)');
+// 	if (count && window.matchMedia('(max-width: 768px)').matches){
+// // 		var but = document.querySelector("#button");
+// // 		but.setAttribute("checked", "checked");
+// 		drop.style.display = "block";
+// 		drop.style.zIndex = "1000";
+// 	}
+// 	else {
+// // 		var but = document.querySelector("#button");
+// // 		but.setAttribute("checked", "false");
+// 		drop.style.display = "none";
+// 		drop.style.zIndex = "0";
+// 	}
+// } 
+
+$(window).on( "load", init);
+
+function init(){
+    var count = false;
+    $('.menu__btn').on('click', () =>{
+        count = !count;
+        if(count){
+            $(".drop-nav").css({'display':'block'});    
+        } else{
+            $(".drop-nav").css({'display':'none'});    
+        }
+        console.log(count);
+    });
