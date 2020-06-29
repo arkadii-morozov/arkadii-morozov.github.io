93edfa43f19657313315e1f640634405d873f016
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-06-29 11:39:12 +0700

    Update button.js
    update show/hide drop-nav list

--- button.js
+++ button.js
@@ -1,25 +1,25 @@
 window.onload = init;
-var i =0;
+// var i =0;
 function init(){
 	var btn = document.querySelector(".menu__btn");
-	btn.onclick = dropMenu;
+	var count = false;
+	btn.onclick = dropMenu(count);
 }
 
-function dropMenu(){
-	i++;
-	console.log(i);
-	var window_size = window.matchMedia('(max-width: 768px)');
-	if ((i%2)>0 && window.matchMedia('(max-width: 768px)').matches){
-		var drop = document.querySelector(".nav");
-		var but = document.querySelector("#button");
-		but.setAttribute("checked", "checked");
+function dropMenu(count){
+	var drop = document.querySelector(".drop-nav");
+	console.log(count);
+	count = !count;
+// 	var window_size = window.matchMedia('(max-width: 768px)');
+	if (count && window.matchMedia('(max-width: 768px)').matches){
+// 		var but = document.querySelector("#button");
+// 		but.setAttribute("checked", "checked");
 		drop.style.display = "block";
 		drop.style.zIndex = "1000";
 	}
 	else {
-		var drop = document.querySelector(".nav");
-		var but = document.querySelector("#button");
-		but.setAttribute("checked", "false");
+// 		var but = document.querySelector("#button");
+// 		but.setAttribute("checked", "false");
 		drop.style.display = "none";
 		drop.style.zIndex = "0";
 	}
