5c6a84de8a2f11b6da330b1026bfe6e2ca296d01
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-07-02 16:57:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// window.onload = init();
-// // var i =0;
-// function init(){
-// 	var count = false;
-// 	var btn = document.querySelector(".menu__btn");
-// 	btn.onclick = dropMenu(count);
-// }
-
-// function dropMenu(count){
-// 	var drop = document.querySelector(".drop-nav");
-// 	console.log(count);
-// 	console.log(drop);
-// 	count = !count;
-// // 	var window_size = window.matchMedia('(max-width: 768px)');
-// 	if (count && window.matchMedia('(max-width: 768px)').matches){
-// // 		var but = document.querySelector("#button");
-// // 		but.setAttribute("checked", "checked");
-// 		drop.style.display = "block";
-// 		drop.style.zIndex = "1000";
-// 	}
-// 	else {
-// // 		var but = document.querySelector("#button");
-// // 		but.setAttribute("checked", "false");
-// 		drop.style.display = "none";
-// 		drop.style.zIndex = "0";
-// 	}
-// } css({'display':'block'}); 
-
 $(window).on( "load", init);
 
 function init(){
@@ -72,10 +44,6 @@ function init(){
             autoplayTimeout:6000,
             dots : false,
             nav:true,
-            navText: [
-                '<span class="fa fa-angle-left fa-4x"></span>',
-                '<span class="fa fa-angle-right fa-4x"></span>'
-            ],
 //             responsive:{
 //                 0:{
 //                     items:1
@@ -89,11 +57,4 @@ function init(){
 //             }
         });
     });
-//     $(".next_button").click(function(){
-//   owl.trigger("next.owl.carousel");
-// });
- 
-// $(".prev_button").click(function(){
-//   owl.trigger("prev.owl.carousel");
-// });
 };
