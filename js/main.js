264b0cc4b8b082d74deef3f930f72248ab334c43
arkadii-morozov <56569955+arkadii-morozov@users.noreply.github.com>
2020-07-16 15:18:59 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,7 +64,8 @@ function init(){
         var top = 0;
         if (height){            
             top = height.substring(height.indexOf('p'), -1);                
-            $('.welcome').css('top',(top-(scrolled*1.35))+'px');
+            console.log(top-(scrolled*1.35));
+            $('.welcome').css('top',(-(scrolled*1.35))+'px');
         }        
     }
     
