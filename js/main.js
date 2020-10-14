767839d4894a7ce448ec6e2c68452bacb8de25f6
arkadii-morozov <arkadii-morozov1@mail.ru>
2020-10-14 17:21:44 +0700

    fixed bugs 14.10.2020

--- js/main.js
+++ js/main.js
@@ -36,13 +36,14 @@ function init(){
 
     $(window).resize(function() {
         var widthDisp = $(window).width()+16;
-        height = $(".welcome").css("top")
+        $('.welcome').css('top','calc(50% - 80px)');
         if ( widthDisp >= 768 ){
              $('.drop-nav').slideUp(20);
              count = false;
         }
     });
-    
+
+    /* === scrolling === */
     $(window).scroll(function(){
         if ($(this).scrollTop() > 150) {
             $('.scrollup').fadeIn();
@@ -50,12 +51,20 @@ function init(){
             $('.scrollup').fadeOut();
         }
     });
-
     $('.scrollup').on('click', function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
-    
+    $('.scrollingPortf').on('click', () => {
+        $("html, body").animate({ scrollTop: $(window).height() }, 600);
+        return false;
+    });
+    $('.scrollingCont').on('click', () => {
+        $("html, body").animate({ scrollTop: $(window).height()*10 }, 600);
+        return false;
+    });
+    /* === end scrolling === */
+
     // parallax    
     $(window).bind('scroll',function(e){
         parallaxScroll();
@@ -63,67 +72,71 @@ function init(){
     function parallaxScroll(){
         var scrolled = $(window).scrollTop();        
         if (height){        
-            var top = height.substring(0, (height.length - 2));                
-            console.log(top);            
-            $('.welcome').css('top',(top-(scrolled*2))+'px');
+            var top = height.substring(0, (height.length - 2));
+            $('.welcome').css('top',(top-(scrolled*1.5))+'px');
         }        
     }
-    
+
     /**
     * Owl Carousel
     */
-    $(function() {
-        var owl = jQuery(".owl-carousel");
-        owl.owlCarousel({
-            loop:true,
-            margin:10,
-            items:1,
-            autoplay:true,
-            autoplayTimeout:6000,
-            dots : true,
-            nav:false,
-            MouseDrag: true,
-            touchDrag: true,
-            navContainer: [$(".nav-panel")]
-//             navText: [
-//                 "<span class='fa fa-angle-left fa-5x'></span>",
-//                 "<span class='fa fa-angle-right fa-5x'></span>"
-//                             ]
-//             responsive:{
-//                 0:{
-//                     items:1
-//                 },
-//                 600:{
-//                     items:3
-//                 },
-//                 1000:{
-//                     items:5
-//                 }
-//             }
-         });
-        $(".owl-dot").click(function(){
-            owl.trigger("stop.owl.autoplay");
-            owl.trigger("play.owl.autoplay");
-        });
-        // Go to the next item
-        $('.owl-button-prev').click(function() {
-            owl.trigger("stop.owl.autoplay");
-            owl.trigger("play.owl.autoplay");
-            owl.trigger('prev.owl.carousel');
-        })
-        // Go to the previous item
-        $('.owl-button-next').click(function() {
-            // With optional speed parameter
-            // Parameters has to be in square bracket '[]'
-            owl.trigger("stop.owl.autoplay");
-            owl.trigger("play.owl.autoplay");
-            owl.trigger('next.owl.carousel');
-        })
-        $(".owl-carousel.owl-drag .owl-item").on("touchstart mousedown", function(e) {
-            // Prevent carousel swipe
-            //e.stopPropagation();
-            owl.trigger("stop.owl.autoplay");
-            owl.trigger("play.owl.autoplay");
-        })
-    });//<- end owl function
+    let pageUrl = window.location.href;
+    let initOwlCarusel = pageUrl.indexOf('about-me.html') + 1;
+    if(initOwlCarusel) {
+
+        $(function () {
+            var owl = jQuery(".owl-carousel");
+            owl.owlCarousel({
+                loop: true,
+                margin: 10,
+                items: 1,
+                autoplay: true,
+                autoplayTimeout: 6000,
+                dots: true,
+                nav: false,
+                MouseDrag: true,
+                touchDrag: true,
+                navContainer: [$(".nav-panel")]
+                //             navText: [
+                //                 "<span class='fa fa-angle-left fa-5x'></span>",
+                //                 "<span class='fa fa-angle-right fa-5x'></span>"
+                //                             ]
+                //             responsive:{
+                //                 0:{
+                //                     items:1
+                //                 },
+                //                 600:{
+                //                     items:3
+                //                 },
+                //                 1000:{
+                //                     items:5
+                //                 }
+                //             }
+            });
+            $(".owl-dot").click(function () {
+                owl.trigger("stop.owl.autoplay");
+                owl.trigger("play.owl.autoplay");
+            });
+            // Go to the next item
+            $('.owl-button-prev').click(function () {
+                owl.trigger("stop.owl.autoplay");
+                owl.trigger("play.owl.autoplay");
+                owl.trigger('prev.owl.carousel');
+            })
+            // Go to the previous item
+            $('.owl-button-next').click(function () {
+                // With optional speed parameter
+                // Parameters has to be in square bracket '[]'
+                owl.trigger("stop.owl.autoplay");
+                owl.trigger("play.owl.autoplay");
+                owl.trigger('next.owl.carousel');
+            })
+            $(".owl-carousel.owl-drag .owl-item").on("touchstart mousedown", function (e) {
+                // Prevent carousel swipe
+                //e.stopPropagation();
+                owl.trigger("stop.owl.autoplay");
+                owl.trigger("play.owl.autoplay");
+            })
+        });//<- end owl function
+    }
 };
